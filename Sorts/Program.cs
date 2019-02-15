03f7a2f957cbf9e59bda7e528efa669c2b1937a7
James T Moore <dukekamono@hotmail.com>
2019-02-15 15:52:55 -0800

    Creation

---
+++ Sorts/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Sorts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			List<int> firstList = new List<int>
+			{
+				3,
+				2,
+				1,
+				4
+			};
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+
+		public static List<int> TopDownMergeSort(List<int> list)
+		{
+			if (list.Count <= 1)
+			{
+				//There is only one item in this list.
+				return list;
+			}
+
+
+			return list;
+		}
+
+		public static List<int> BottomUpMergeSort(List<int> list)
+		{
+			return list;
+		}
+    }
+}
