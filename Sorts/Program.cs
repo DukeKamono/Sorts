d5ee4a08341f1d164f03d14f78eeccb84262aa06
James T Moore <dukekamono@hotmail.com>
2019-02-15 17:19:04 -0800

    Merge sort v1

--- Sorts/Program.cs
+++ Sorts/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 
 namespace Sorts
@@ -16,9 +17,11 @@ namespace Sorts
 				4
 			};
 
+			var test = TopDownMergeSort(firstList);
 
+            test.ForEach(Console.WriteLine);
 
-            Console.WriteLine("Hello World!");
+			Console.ReadLine();
         }
 
 		public static List<int> TopDownMergeSort(List<int> list)
@@ -29,8 +32,59 @@ namespace Sorts
 				return list;
 			}
 
+			var leftSide = new List<int>();
+			var rightSide = new List<int>();
 
-			return list;
+			//Split the list to two smaller lists
+			foreach (var item in list.Select((value, index) => new { index, value }))
+			{
+				if (item.index < (list.Count() / 2))
+				{
+					leftSide.Add(item.value);
+				}
+				else
+				{
+					rightSide.Add(item.value);
+				}
+			}
+
+			leftSide = TopDownMergeSort(leftSide);
+			rightSide = TopDownMergeSort(rightSide);
+
+			return Merge(leftSide, rightSide);
+		}
+
+		public static List<int> Merge(List<int> firstList, List<int> secondList)
+		{
+			var finalList = new List<int>();
+
+			while (firstList.Count > 0 && secondList.Count > 0)
+			{
+				if (firstList.First() <= secondList.First())
+				{
+					finalList.Add(firstList.First());
+					firstList.Remove(firstList.First());
+				}
+				else
+				{
+					finalList.Add(secondList.First());
+					secondList.Remove(secondList.First());
+				}
+			}
+
+			while (firstList.Count > 0)
+			{
+				finalList.Add(firstList.First());
+				firstList.Remove(firstList.First());
+			}
+
+			while (secondList.Count > 0)
+			{
+				finalList.Add(secondList.First());
+				secondList.Remove(secondList.First());
+			}
+
+			return finalList;
 		}
 
 		public static List<int> BottomUpMergeSort(List<int> list)
